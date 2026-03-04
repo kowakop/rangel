f8de944beb0fac1e0a46ca2ec503be6950bec85c
Kowa <kibecomkibe@gmail.com>
2026-03-04 10:07:19 -0300

    pronto

--- love letter 2.js
+++ love letter 2.js
@@ -1,3 +1,28 @@
+//envelope
+
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+
 
 window.onresize=set_width;
 function set_width() {
@@ -23,6 +48,7 @@ function set_width() {
   shigh=sh_min;
 }
 
+//penguin
 window.onscroll=set_scroll;
 function set_scroll() {
   if (typeof(self.pageYOffset)=='number') {
